86700b18a7371c8a9ba0007b92dea5047fff9e28
Khan Sharukh <khan_sharukh@outlook.com>
2020-11-05 10:30:14 +0530

    Remove - registration and forgot password url

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,14 +31,7 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
                 <x-jet-button class="ml-4">
                     {{ __('Login') }}
                 </x-jet-button>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,11 +27,7 @@
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Admin Login</a>
                     @endif
                 </div>
             @endif
